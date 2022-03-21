942cfada096b765a4bc114c1dd8ef3ea725d6e2f
Tobias Rødahl Thingnes <tobiasth@stud.ntnu.no>
2022-03-21 10:01:52 +0100

    Started writing tests

--- OnionProxy/src/test/kotlin/DiffieHellmanUtilTest.kt
+++ OnionProxy/src/test/kotlin/DiffieHellmanUtilTest.kt
@@ -1,14 +1,16 @@
 import edu.ntnu.tobiasth.onionproxy.util.DiffieHellmanUtil
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import javax.crypto.interfaces.DHPrivateKey
 import kotlin.test.assertContentEquals
 
 internal class DiffieHellmanUtilTest {
     @Test
+    @DisplayName("Key exchange with two key pairs gives the same shared secret.")
     fun testSharedSecretIsCorrect() {
         val aliceKeyPair = DiffieHellmanUtil.getKeyPair()
         val bobKeyPair = DiffieHellmanUtil.getKeyPair()
-        val params = (aliceKeyPair.public as DHPrivateKey).params
+        val params = (aliceKeyPair.private as DHPrivateKey).params
 
         val aliceSharedSecret = DiffieHellmanUtil.getSharedSecret(aliceKeyPair.private, bobKeyPair.public, params)
         val bobSharedSecret = DiffieHellmanUtil.getSharedSecret(bobKeyPair.private, aliceKeyPair.public, params)
