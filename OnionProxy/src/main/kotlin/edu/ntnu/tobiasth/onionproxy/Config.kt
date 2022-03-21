ade96333cd0d7c828b57b62bda702e660a1d96dc
Tobias Rødahl Thingnes <tobiasth@stud.ntnu.no>
2022-03-21 16:07:57 +0100

    Use router directory from config in socket util

--- OnionProxy/src/main/kotlin/edu/ntnu/tobiasth/onionproxy/Config.kt
+++ OnionProxy/src/main/kotlin/edu/ntnu/tobiasth/onionproxy/Config.kt
@@ -1,6 +1,9 @@
 package edu.ntnu.tobiasth.onionproxy
 
+import edu.ntnu.tobiasth.onionproxy.onion.OnionRouterDirectory
+import edu.ntnu.tobiasth.onionproxy.onion.OnionRouterInfo
 import edu.ntnu.tobiasth.onionproxy.util.DiffieHellmanUtil
+import java.net.InetAddress
 import java.security.KeyPair
 
 class Config {
@@ -12,5 +15,10 @@ class Config {
         const val BUFFER_SIZE: Int = 4096
 
         val PROXY_KEY: KeyPair = DiffieHellmanUtil.getKeyPair()
+        val ROUTER_DIRECTORY: OnionRouterDirectory = OnionRouterDirectory(listOf(
+            OnionRouterInfo(InetAddress.getLoopbackAddress(), 1111),
+            OnionRouterInfo(InetAddress.getLoopbackAddress(), 2222),
+            OnionRouterInfo(InetAddress.getLoopbackAddress(), 3333),
+        ))
     }
 }
--- OnionProxy/src/main/kotlin/edu/ntnu/tobiasth/onionproxy/util/SocketUtil.kt
+++ OnionProxy/src/main/kotlin/edu/ntnu/tobiasth/onionproxy/util/SocketUtil.kt
@@ -4,7 +4,6 @@ import edu.ntnu.tobiasth.onionproxy.Config
 import edu.ntnu.tobiasth.onionproxy.onion.OnionCircuit
 import edu.ntnu.tobiasth.onionproxy.onion.stream.OnionInputStream
 import edu.ntnu.tobiasth.onionproxy.onion.stream.OnionOutputStream
-import edu.ntnu.tobiasth.onionproxy.onion.OnionRouterDirectory
 import mu.KotlinLogging
 import java.io.*
 import java.net.InetAddress
@@ -25,9 +24,8 @@ class SocketUtil {
         fun getSocketStreams(socket: Socket): Pair<InputStream, OutputStream> {
             logger.debug { "Using ${if (Config.ONION_ENABLED) "onion" else "direct"} streams." }
             return if (Config.ONION_ENABLED) {
-                val circuit = OnionUtil.createCircuit(Config.ONION_CIRCUIT_SIZE, OnionRouterDirectory())
+                val circuit = OnionUtil.createCircuit(Config.ONION_CIRCUIT_SIZE, Config.ROUTER_DIRECTORY)
                 Pair(getOnionInput(socket, circuit), getOnionOutput(socket, circuit))
-                TODO("Get router directory from somewhere.")
             } else {
                 Pair(getDirectInput(socket), getDirectOutput(socket))
             }
