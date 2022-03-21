963e8d0ce869c29d927e6074860c8f58c0039d41
Tobias Rødahl Thingnes <tobiasth@stud.ntnu.no>
2022-03-21 16:17:48 +0100

    Added method to pick random routers for circuit

--- OnionProxy/src/main/kotlin/edu/ntnu/tobiasth/onionproxy/util/OnionUtil.kt
+++ OnionProxy/src/main/kotlin/edu/ntnu/tobiasth/onionproxy/util/OnionUtil.kt
@@ -2,11 +2,32 @@ package edu.ntnu.tobiasth.onionproxy.util
 
 import edu.ntnu.tobiasth.onionproxy.onion.OnionCircuit
 import edu.ntnu.tobiasth.onionproxy.onion.OnionRouterDirectory
+import edu.ntnu.tobiasth.onionproxy.onion.OnionRouterInfo
 
 class OnionUtil {
     companion object {
+        private var id = 0
+
         fun createCircuit(size: Int, directory: OnionRouterDirectory): OnionCircuit {
-            TODO()
+            val routers = arrayListOf<OnionRouterInfo>()
+            routers.addAll(directory.routers)
+
+            val firstRouter = routers.random()
+            routers.remove(firstRouter)
+            val circuit = OnionCircuit(id++, firstRouter)
+
+            try {
+                repeat(size - 1) {
+                    val randomRouter = routers.random()
+                    routers.remove(randomRouter)
+                    circuit.extend(randomRouter)
+                }
+            }
+            catch(e: NoSuchElementException) {
+                throw IllegalStateException("Not enough routers to create circuit of size $size")
+            }
+
+            return circuit
         }
     }
 }
